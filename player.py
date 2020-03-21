8170de13ffe35bd90e1e0e94a7f0a5697e07b359
andrewHolsaeter <andrew.m.holsaeter@gmail.com>
2020-03-21 23:53:58 +0100

    Added some more functions and hack to wait for login so that playlist gets added on startup

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import subprocess
 import re
+import time
 
 server_cmd = 'cmd'
 player_cmd = 'mpc'
@@ -15,6 +16,7 @@ class SpotifyPlayer():
   full_cmd = [cmd]
   for arg in args:
    full_cmd.append(arg)
+  print(full_cmd)
   out = subprocess.Popen(full_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
   output = out.communicate()
   return output
@@ -24,23 +26,23 @@ class SpotifyPlayer():
  #  Bebe Rexha;Asketa & Natan Chaim;The Chainsmokers - Call You Mine - Asketa & Natan Chaim Remix
  #  [playing] #22/36   2:47/3:03 (91%)
  #  volume: 25%   repeat: off   random: off   single: off   consume: off
-
   output = out[0].decode('utf-8').splitlines()
+  # First time playing/pausing doesn't have status returned? 
+  if isinstance(output, list) and len(output) == 3:
+  	# Don't think I can individually parse artist, song, album, as '-' in any of these would break things
+  	playing = output[0]
+  	song_status = output[1]
+  	mpc_status = output[2]
 
-  # Don't think I can individually parse artist, song, album, as '-' in any of these would break things
-  playing = output[0]
-  song_status = output[1]
-  mpc_status = output[2]
+  	# Update current song
+  	self.current_song = playing
 
-  # Update current song
-  self.current_song = playing
+  	# Get status of song
+  	status = re.search(r"\[(.*?)\]",song_status).group(1)
+  	self.status = status
 
-  # Get status of song
-  status = re.search(r"\[(.*?)\]",song_status).group(1)
-  self.status = status
-
-  # MPC status
-  self.mpc_status = mpc_status
+  	# MPC status
+  	self.mpc_status = mpc_status
 
 #  print(playing)
 #  print(song_status, status)
@@ -57,4 +59,47 @@ class SpotifyPlayer():
  def pause(self):
   output = self.run(player_cmd, 'pause')
   self.update_status(output)
+
+ def toggle(self):
+  # TODO, change to 'mpc toggle'
+  # For now, we assume
+  if self.status == "paused":
+   self.play()
+  elif self.status == "playing":
+   self.pause()
+  else:
+   print("Unkown status state")
+
+ def clear(self):
+  output = self.run(player_cmd, 'clear')
+
+ def add_playlist(self, playlist="2wEdMantM5JEHCt01A6y64"):
+  str = "spotify:playlist:"+playlist
+  output = self.run(player_cmd, 'add', str)
+
+ def shuffle(self):
+  output = self.run(player_cmd, 'shuffle')
+
+ def set_volume(self, val):
+  output = self.run(player_cmd, 'volume', str(val))
+
+ def wait_for_connection(self):
+  max_time_to_wait = 30
+  t = 0
+  while True:
+   output = self.run(player_cmd, 'status')
+   status = output[0].decode('utf-8')
+   if status != "mpd error: Connection refused\n":
+    break
+   elif t > max_time_to_wait:
+    print(f"Waited max {max_time_to_wait} seconds")
+    break
+   print("Waiting for mpc to initialize...")
+   t += 1
+   time.sleep(1)
+
+ def idle(self):
+  print("Starting Idle")
+  output = self.run(player_cmd, 'idle')
+  print(f"Done Idling: {output}")
 
